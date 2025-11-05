78d2f4304d3ec9f9d3489501db4c787ddcdf06fe
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2025-11-05 20:22:58 +0400

    Socials update

--- src/components/ui/Socials/Socials.tsx
+++ src/components/ui/Socials/Socials.tsx
@@ -1,24 +1,24 @@
 import clsx from 'clsx';
 import type { FC, HTMLAttributes } from 'react';
 
-import DiscordIcon from '../../../assets/icons/discord.svg?react';
+// import DiscordIcon from '../../../assets/icons/discord.svg?react';
 import XIcon from '../../../assets/icons/x.svg?react';
 import TelegramIcon from '../../../assets/icons/telegram.svg?react';
 
 const Socials: FC<HTMLAttributes<HTMLDivElement>> = ({ className }) => {
   return (
     <div className={clsx(className, 'flex gap-5')}>
+      {/*<a*/}
+      {/*  className={'size-9.5 text-grey-light hover:text-grey-lightest tr-d-all'}*/}
+      {/*  href={'https://discord.gg/'}*/}
+      {/*  target={'_blank'}*/}
+      {/*  rel={'noreferrer nofollow'}*/}
+      {/*>*/}
+      {/*  <DiscordIcon />*/}
+      {/*</a>*/}
       <a
         className={'size-9.5 text-grey-light hover:text-grey-lightest tr-d-all'}
-        href={'https://discord.gg/'}
-        target={'_blank'}
-        rel={'noreferrer nofollow'}
-      >
-        <DiscordIcon />
-      </a>
-      <a
-        className={'size-9.5 text-grey-light hover:text-grey-lightest tr-d-all'}
-        href={'https://x.com/'}
+        href={'https://x.com/StarFlipGaming'}
         target={'_blank'}
         rel={'noreferrer nofollow'}
       >
